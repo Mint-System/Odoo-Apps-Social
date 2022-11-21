da4cd522db1a4b99618356aaea2711801ba37892
janikvonrotz <contact@janikvonrotz.ch>
2022-11-21 20:57:27 +0100

    [mail_no_autofollow] do not subscribe if not author

--- mail_no_autofollow/models/mail_thread.py
+++ mail_no_autofollow/models/mail_thread.py
@@ -4,7 +4,7 @@ _logger = logging.getLogger(__name__)
 
 
 class MailThread(models.AbstractModel):
-    _inherit = "mail.thread"
+    _inherit = 'mail.thread'
 
     """
      - `mail_create_nosubscribe`: at create or message_post, do not subscribe
@@ -22,4 +22,8 @@ class MailThread(models.AbstractModel):
 
     @api.returns('mail.message', lambda value: value.id)
     def message_post(self, **kwargs):
-        return super(MailThread, self.with_context(mail_post_autofollow=False)).message_post(**kwargs)
+        message = super(MailThread, self.with_context(mail_post_autofollow=False)).message_post(**kwargs)
+        # If current user is not author, unsubscribe the author
+        if message.author_id != self.env.user.partner_id:
+          self.message_unsubscribe(partner_ids=[message.author_id.id])
+        return message
