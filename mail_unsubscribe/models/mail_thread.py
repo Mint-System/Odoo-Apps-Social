50ab51c502683c8f216f81d93ddce00ef64cda77
Janik von Rotz <login@janikvonrotz.ch>
2023-04-18 21:02:04 +0200

    [mail_unsubscribe] fix

--- mail_unsubscribe/models/mail_thread.py
+++ mail_unsubscribe/models/mail_thread.py
@@ -9,17 +9,20 @@ class MailThread(models.AbstractModel):
 
     def _unsubscribe_followers(self):
         ignore_current_partner = ast.literal_eval(self.env['ir.config_parameter'].sudo().get_param('mail_unsubscribe.ignore_current_partner', 'False'))
-        for mt in self:
-            current_partner_id = self.env.user.partner_id
-            message_partner_ids = mt.message_partner_ids
+        for rec in self:
+            current_partner_id = rec.env.user.partner_id
+            message_partner_ids = rec.message_partner_ids
             if ignore_current_partner:
                 message_partner_ids = message_partner_ids.filtered(lambda p: p != current_partner_id)
-            mt.message_unsubscribe(message_partner_ids.ids)
+            rec.message_unsubscribe(message_partner_ids.ids)
 
     @api.returns('mail.message', lambda value: value.id)
     def message_post(self, **kwargs):
-        self._unsubscribe_followers()
         mail_post_autofollow = ast.literal_eval(self.env['ir.config_parameter'].sudo().get_param('mail_unsubscribe.mail_post_autofollow', 'False'))
         mail_create_nosubscribe = ast.literal_eval(self.env['ir.config_parameter'].sudo().get_param('mail_unsubscribe.mail_create_nosubscribe', 'False'))
-        res = super(MailThread, self.with_context(mail_post_autofollow=mail_post_autofollow, mail_create_nosubscribe=mail_create_nosubscribe)).message_post(**kwargs)
-        return res
+        if self._name != 'mail.channel':
+            self._unsubscribe_followers()
+            _self = self.with_context(mail_post_autofollow=mail_post_autofollow, mail_create_nosubscribe=mail_create_nosubscribe)
+            return super(MailThread, _self).message_post(**kwargs)
+        else:
+            return super(MailThread, self).message_post(**kwargs)
