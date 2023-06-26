f50f3f3ccfb5515f1c45a945743b8c22847a3754
Janik von Rotz <login@janikvonrotz.ch>
2023-06-26 09:32:19 +0200

    [mail_unsubscribe] migrate

---
+++ README.md
@@ -0,0 +1,17 @@
+# Odoo Apps: Social
+
+Collection of social model related modules.
+
+## Usage
+
+Clone module into Odoo addon directory.
+
+```bash
+git clone git@github.com:mint-system/odoo-apps-social.git ./addons/social
+```
+
+## Available modules
+
+| Module | Summary |
+| --- | --- |
+| [mail_unsubscribe](mail_unsubscribe) |         Auto unsubscribe followers before sending a message. |
---
+++ mail_unsubscribe/models/res_config_settings.py
@@ -0,0 +1,11 @@
+from odoo import api, fields, models, _
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    ignore_current_partner = fields.Boolean(config_parameter='mail_unsubscribe.ignore_current_partner')
+    mail_post_autofollow = fields.Boolean(config_parameter='mail_unsubscribe.mail_post_autofollow')
+    mail_create_nosubscribe = fields.Boolean(config_parameter='mail_unsubscribe.mail_create_nosubscribe')
